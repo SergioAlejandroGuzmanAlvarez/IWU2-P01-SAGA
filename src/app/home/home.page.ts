dca776b4aeee4845a8f7c016590601dff729d9ff
Sergio Guzman <sealguzmanal@ittepic>
2023-03-17 22:33:19 -0700

    Version 0.0.2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public products = [
+    {
+      name:"Coca Cola 600ml",
+      price:20,
+      type: "Abarrotes"
+    },
+    {
+      name:"Jabon Zote",
+      price:30,
+      type: "Limpieza"
+    },
+    {
+      name:"Croquetes 50kg",
+      price:400,
+      type: "Mascotas"
+    },
+    {
+      name:"Zucaritas 500g",
+      price:100,
+      type: "Abarrotes"
+    }
+];
   constructor() {}
+  getColor(type: String) : string{
+    let color = "";
 
+    switch(type){
+      case "Abarrotes":
+        color = "primary";
+        break;
+      case "Limpieza":
+        color = "success";
+        break;
+      case "Mascotas":
+        color = "warning";
+        break;  
+      default:
+        break;
+    }
+    return color;
+  }
 }
